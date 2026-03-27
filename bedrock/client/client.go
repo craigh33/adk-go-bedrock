e8916003c0bf5b0f55104818aab58c8fc0048e58
Michael Lindsay <michael.lindsay@n-able.com>
2026-03-27 09:27:55 +0000

    feat(client): adding initial otel to client

--- bedrock/client/client.go
+++ bedrock/client/client.go
@@ -6,6 +6,9 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/service/bedrockruntime"
 	"github.com/aws/aws-sdk-go-v2/service/bedrockruntime/types"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
 )
 
 // StreamReader is the subset of the Converse stream API used by the model provider.
@@ -29,13 +32,31 @@ type RuntimeAPI interface {
 	) (StreamReader, error)
 }
 
+// Option configures an adapter created with [NewFromClient].
+type Option func(*adapter)
+
+// WithTracer attaches an OpenTelemetry [trace.Tracer] used to record spans for
+// [RuntimeAPI.Converse] and [RuntimeAPI.ConverseStream].
+func WithTracer(tracer trace.Tracer) Option {
+	return func(a *adapter) {
+		a.tracer = tracer
+	}
+}
+
 type adapter struct {
-	inner *bedrockruntime.Client
+	inner  *bedrockruntime.Client
+	tracer trace.Tracer
 }
 
 // NewFromClient wraps a [bedrockruntime.Client] as [RuntimeAPI].
-func NewFromClient(c *bedrockruntime.Client) RuntimeAPI {
-	return &adapter{inner: c}
+func NewFromClient(c *bedrockruntime.Client, opts ...Option) RuntimeAPI {
+	a := &adapter{inner: c}
+	for _, opt := range opts {
+		if opt != nil {
+			opt(a)
+		}
+	}
+	return a
 }
 
 func (c *adapter) Converse(
@@ -43,6 +64,22 @@ func (c *adapter) Converse(
 	params *bedrockruntime.ConverseInput,
 	optFns ...func(*bedrockruntime.Options),
 ) (*bedrockruntime.ConverseOutput, error) {
+	if c.tracer != nil {
+		ctx, span := c.tracer.Start(ctx, "bedrock.Converse",
+			trace.WithSpanKind(trace.SpanKindClient),
+		)
+		defer span.End()
+		if params != nil && params.ModelId != nil && *params.ModelId != "" {
+			span.SetAttributes(attribute.String("aws.bedrock.model_id", *params.ModelId))
+		}
+		out, err := c.inner.Converse(ctx, params, optFns...)
+		if err != nil {
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
+			return nil, err
+		}
+		return out, nil
+	}
 	return c.inner.Converse(ctx, params, optFns...)
 }
 
@@ -51,6 +88,22 @@ func (c *adapter) ConverseStream(
 	params *bedrockruntime.ConverseStreamInput,
 	optFns ...func(*bedrockruntime.Options),
 ) (StreamReader, error) {
+	if c.tracer != nil {
+		ctx, span := c.tracer.Start(ctx, "bedrock.ConverseStream",
+			trace.WithSpanKind(trace.SpanKindClient),
+		)
+		defer span.End()
+		if params != nil && params.ModelId != nil && *params.ModelId != "" {
+			span.SetAttributes(attribute.String("aws.bedrock.model_id", *params.ModelId))
+		}
+		out, err := c.inner.ConverseStream(ctx, params, optFns...)
+		if err != nil {
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
+			return nil, err
+		}
+		return out.GetStream(), nil
+	}
 	out, err := c.inner.ConverseStream(ctx, params, optFns...)
 	if err != nil {
 		return nil, err
